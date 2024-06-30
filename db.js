e7034a07c0e6b90bfbd30333486ed650879ce2d1
Jash Kevadiya <105188905+Jash9603@users.noreply.github.com>
2024-06-30 18:15:37 +0530

    initial commit 3

--- db.js
+++ db.js
@@ -1,34 +1,44 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
-//const mongoURL = 'mongodb://localhost:27017/hotels';
-
 
 const mongoURL = process.env.DB_URL;
 
-mongoose.connect(mongoURL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+const connectionOptions = {
+  tls: true,
+  tlsInsecure: false  // Ensure this is set to false for production
+};
+
+const connectWithRetry = () => {
+  console.log('MongoDB connection with retry');
+  mongoose.connect(mongoURL, connectionOptions)
+    .then(() => {
+      console.log('MongoDB is connected');
+    })
+    .catch(err => {
+      console.log('MongoDB connection unsuccessful, retry after 5 seconds. ', err);
+      setTimeout(connectWithRetry, 5000);  // Retry after 5 seconds
+    });
+};
+
+connectWithRetry();
 
 const db = mongoose.connection;
 
 db.on('connected', () => {
-  console.log('connection established');
+  console.log('MongoDB connection established');
 });
 
 db.on('error', (err) => {
-  console.log('connection error: ' + err);
+  console.error('MongoDB connection error: ' + err);
 });
 
 db.on('disconnected', () => {
-  console.log('connection disconnected');
+  console.log('MongoDB connection disconnected');
 });
 
 db.on('reconnected', () => {
-  console.log('connection reconnected');
+  console.log('MongoDB connection reconnected');
 });
-
-
 
 mongoose.set('debug', true);
 
