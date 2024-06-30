410226cf9a7504cf9889e7af3f9814635d324276
Jash Kevadiya <105188905+Jash9603@users.noreply.github.com>
2024-06-30 18:04:00 +0530

    initial commit 2

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const app = express()
 const db = require('./db');
+require('dotenv').config();
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json()); // this line store the data in req.body
@@ -16,8 +17,8 @@ app.use('/person', personRoute);
 app.use('/menu', menuRoute);
 
 
-
-app.listen(3000 , () => {
+const port = process.env.PORT || 3000;
+app.listen(port , () => {
   console.log('listening 3000')
 })
 
